262e32339f94cfb3dc66dd495482acb34896a1f1
Aishu030 <aishuramesh031@gmail.com>
2023-08-10 22:33:42 +0530

    commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,10 +13,10 @@ CourseCode nvarchar(50),
 RegistrationDate date,
 constraint pk_SR primary key(StudentId, CourseCode))
 
-insert into StudentRegistrations values(999, 'EC304', '04/08/2023')
-insert into StudentRegistrations values(998, 'EC302','08/09/2023')
-insert into StudentRegistrations values(997, 'EC302','04/06/2023')
-insert into StudentRegistrations values(996, 'EC301', '04/06/2023')
-insert into StudentRegistrations values(999, 'EC305', '10/08/2023')
+insert into StudentRegistrations values(1, 'ECE301', '04/08/2023')
+insert into StudentRegistrations values(2, 'ECE302','08/09/2023')
+insert into StudentRegistrations values(3, 'ECE303','04/06/2023')
+insert into StudentRegistrations values(4, 'ECE304', '04/06/2023')
+insert into StudentRegistrations values(5, 'ECE305', '10/08/2023')
 
 select * from StudentRegistrations
